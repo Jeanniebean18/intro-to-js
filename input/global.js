cbd5fc7a439030a067ebd09ab84ee02f75592059
Jeannie <jeannie@kickpunchcreative.com>
2015-07-06 14:40:04 -0500

    section 4 complete.

--- input/global.js
+++ input/global.js
@@ -1,15 +1,19 @@
-var name = prompt("What is your name?");
+
+//prompting user for inputs of variables.
 var age = prompt("What is your age?");
+var name = prompt("What is your name?");
 var hometown = prompt("Where is your hometown?");
 var favNumber = prompt("What is your favorite number?");
-
+//retelling the user what they entered.
 alert("Your name is " + name);
 alert("Your age is " + age);
 alert("Your hometown is " + hometown);
 alert("Your favorite number is " + favNumber);
-
+//reporting to the console what was entered.
 console.log("User name is " + name);
 console.log("User age is " + age);
 console.log("User hometown is " + hometown);
 console.log("User favorite number is " + favNumber);
+//checking to see what type of datatype the variable age is.
+console.log(typeof(age));
 
