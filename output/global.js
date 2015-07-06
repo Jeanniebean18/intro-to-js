adf4ec603251b417e5991a39b0471f9418f8a136
Jeannie <jeannie@kickpunchcreative.com>
2015-07-06 11:29:22 -0500

    output

---
+++ output/global.js
@@ -0,0 +1,10 @@
+//pop-up alert to user
+alert("Hi, Jeannie");
+// console logs are for debugging. user does not see these.
+console.log("My fav breakfast is Jimmy's Eggs.");
+console.log("I live with two cats.");
+console.log("I eat lots of food at Darios.");
+console.log("I never take naps.");
+console.log("Naps are for the lazy.");
+//pop-up alert to user saying goodbye.
+alert("Goodbye, Jeannie");
