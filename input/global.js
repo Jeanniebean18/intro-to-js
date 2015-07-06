52cb0e01477279fcde0289b2fdc96838ac1f220c
Jeannie <jeannie@kickpunchcreative.com>
2015-07-06 14:09:34 -0500

    section 4

---
+++ input/global.js
@@ -0,0 +1,15 @@
+var name = prompt("What is your name?");
+var age = prompt("What is your age?");
+var hometown = prompt("Where is your hometown?");
+var favNumber = prompt("What is your favorite number?");
+
+alert("Your name is " + name);
+alert("Your age is " + age);
+alert("Your hometown is " + hometown);
+alert("Your favorite number is " + favNumber);
+
+console.log("User name is " + name);
+console.log("User age is " + age);
+console.log("User hometown is " + hometown);
+console.log("User favorite number is " + favNumber);
+
