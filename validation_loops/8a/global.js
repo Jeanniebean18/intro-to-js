a9d3de0165a28fb2ae87d13c6062d092353b4d8a
Jeannie <jeannie@kickpunchcreative.com>
2015-07-07 10:14:48 -0500

    8

--- validation_loops/8a/global.js
+++ validation_loops/8a/global.js
@@ -1,13 +1,14 @@
 
 var password = "peanutbutter";
 
-var user_input = prompt("Please enter a password");
+var user_input = (prompt("Please enter a password")).toLowerCase();
 
 
 while (password != user_input)
-{
+{ counter = 1
   alert("wrong password, try again.");
   var user_input = prompt("Please enter a password");
+  counter++;
 }
 
- alert("You got it!");
+ alert("You got it! and it took you " + counter + " tries");
