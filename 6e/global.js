9957c3d4b56bdf32b91e335185160be4132d066b
Jeannie <jeannie@kickpunchcreative.com>
2015-07-06 19:52:48 -0500

    6e

--- 6e/global.js
+++ 6e/global.js
@@ -1,42 +1,44 @@
 
 var user_age = parseInt(prompt("how old are you?"));
 
-
-
 if (user_age >= 25) 
 {
   alert("You can rent a car");
 }
 
-if (user_age >= 21)
+else if (user_age <= 24 && user_age >= 21)
 {
   alert("Please don't drink and drive");
 }
 
-if (user_age >= 16)
+else if (user_age <= 20 && user_age >= 16)
 {
   alert("You're eligible for a driver's license");
 }
 
-if (user_age <= 15 && user_age >= 0)
+else if (user_age <= 15 && user_age >= 0)
 {
   alert("You can't drive yet, but you'll be able to in a few years");
 }
 
-if (user_age < 0)
+else if (user_age < 0)
 {
   alert("You haven't been born yet");
 }
 
-if (isNaN(user_age))
+else if (isNaN(user_age))
 {
   alert("Sorry, but that's not a number");
 }
 
+else
+{
+  alert("that's impossible");
+}
 
-// Age 25 and older: "You can rent a car"
-// Age 21 and older: "Please don't drink and drive"
-// Age 16 and older: "You're eligible for a driver's license"
+// /Age 25 and older: "You can rent a car"
+// Age 21 - 24: "Please don't drink and drive"
+// Age 16 - 20: "You're eligible for a driver's license"
 // Age 0 - 15: "You can't drive yet, but you'll be able to in a few years"
 // Any negative number: "You haven't been born yet"
 // NaN: "Sorry, but that's not a number"
